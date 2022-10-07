44a896b24a95861a5bfb85bdc33498f791c9d2d4
Saulo Yamade <saulo.yamade@tecsysbrasil.com.br>
2022-10-07 17:26:10 -0300

    Exemples

---
+++ cap2_littlekid.py
@@ -0,0 +1,9 @@
+name = 'Jose'
+age = 15
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice, Kiddo')
+else:
+    print('You are neither Alice nor a little kid')
